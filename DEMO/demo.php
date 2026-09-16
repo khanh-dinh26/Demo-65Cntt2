3d7ea759588a72aa731b0cdc3d9110365e0901b6
Nguyễn Đình Khánh <khankhanh683@gmail.com>
2026-09-16 15:23:11 +0700

    first commit

---
+++ DEMO/demo.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1" cellpadding="8" cellspacing="0" align="center">
+        <tr align="center" bgcolor="#f2f2f2">
+            <th>STT</th>
+            <th>Họ và tên</th>
+            <th>MSSV</th>
+        </tr>
+
+        <?php
+            $thanh_vien = [
+                ["ten" => "Nguyễn Đình Khánh", "mssv" => "65131460"],
+                ["ten" => "Trần Thị B",  "mssv" => "00000000"],
+                ["ten" => "Lê Văn C",    "mssv" => "00000000"],
+                ["ten" => "Nguyễn Văn A", "mssv" => "00000000"],
+                ["ten" => "Trần Thị B",  "mssv" => "00000000"],
+                ["ten" => "Lê Văn C",    "mssv" => "00000000"],
+            ];
+
+            $stt = 1;
+            foreach ($thanh_vien as $tv) {
+                echo "<tr align='center'>";
+                echo "<td>" . $stt++ . "</td>";
+                echo "<td>" . $tv['ten'] . "</td>";
+                echo "<td>" . $tv['mssv'] . "</td>";
+                echo "</tr>";
+            }
+        ?>
+    </table>
+</body>
+</html>
